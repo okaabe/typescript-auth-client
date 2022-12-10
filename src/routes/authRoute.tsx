ccb05d8cd4e4b4779c244967b1bcb02eb93bc891
tosuki <okaabe2006@gmail.com>
2022-12-10 19:10:57 -0300

    Added auth route - auth route is routes that the user needs to be authenticated to have access

---
+++ src/routes/authRoute.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+import { Navigate } from "react-router-dom"
+import { isAuthenticated, useSession } from "../hooks/session"
+
+const AuthRoute = (props: { children: React.ReactNode }) => {
+  const session = useSession()
+
+  if (isAuthenticated(session)) {
+    return <>{ props.children }</>
+  }
+
+  return <Navigate to="/"/>
+}
+
+export default AuthRoute
